5fc820956fb0978a1e25b49ce632b4d717139af8
emmanuel-h <emmanuelh@openmailbox.org>
2017-10-25 17:48:10 +0200

    interface facade utilisateur

--- CavardageModele/src/main/java/ejbs/MaFacadeUtilisateur.java
+++ CavardageModele/src/main/java/ejbs/MaFacadeUtilisateur.java
@@ -1,7 +1,21 @@
 package ejbs;
 
+import entities.Trajet;
+import entities.Vehicule;
+
 import javax.ejb.Local;
+import java.util.List;
 
 @Local
 public interface MaFacadeUtilisateur {
+
+    boolean reserverPlace(String login, int idTrajet, int nbPlaces);
+    boolean faireCommentaire(String login, int idTrajet);
+    boolean donnerNote(String login, int idTrajet);
+    Trajet proposerTrajet(String login, String villeDepart, String villeArrivee, List<String> etapes, String date);
+    Vehicule ajouterVehicule(String login, String nomVehicule, String modele, int idGabarit, int nbPlaces);
+    boolean annulerTrajet(String login, int idTrajet);
+    boolean avoirReservations(String login, int idTrajet);
+    boolean refuserReservation(String login, int idReservation);
+    boolean accepterReservation(String login, int idreservation);
 }
--- CavardageModele/src/main/java/ejbs/MaFacadeUtilisateurBean.java
+++ CavardageModele/src/main/java/ejbs/MaFacadeUtilisateurBean.java
@@ -1,4 +1,55 @@
 package ejbs;
 
+import entities.Trajet;
+import entities.Vehicule;
+
+import javax.ejb.Stateless;
+import java.util.List;
+
+@Stateless
 public class MaFacadeUtilisateurBean implements MaFacadeUtilisateur {
+    @Override
+    public boolean reserverPlace(String login, int idTrajet, int nbPlaces) {
+        return false;
+    }
+
+    @Override
+    public boolean faireCommentaire(String login, int idTrajet) {
+        return false;
+    }
+
+    @Override
+    public boolean donnerNote(String login, int idTrajet) {
+        return false;
+    }
+
+    @Override
+    public Trajet proposerTrajet(String login, String villeDepart, String villeArrivee, List<String> etapes, String date) {
+        return null;
+    }
+
+    @Override
+    public Vehicule ajouterVehicule(String login, String nomVehicule, String modele, int idGabarit, int nbPlaces) {
+        return null;
+    }
+
+    @Override
+    public boolean annulerTrajet(String login, int idTrajet) {
+        return false;
+    }
+
+    @Override
+    public boolean avoirReservations(String login, int idTrajet) {
+        return false;
+    }
+
+    @Override
+    public boolean refuserReservation(String login, int idReservation) {
+        return false;
+    }
+
+    @Override
+    public boolean accepterReservation(String login, int idreservation) {
+        return false;
+    }
 }
