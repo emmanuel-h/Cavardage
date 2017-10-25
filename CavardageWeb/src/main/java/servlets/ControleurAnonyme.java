3f85561dd548bcb629612a66f983ab1881784cea
benoitgallet <benoit.gallet@live.fr>
2017-10-25 19:14:08 +0200

    controleurs pour tout le monde

---
+++ CavardageWeb/src/main/java/servlets/ControleurAnonyme.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("ControleurAnonyme")
+public class ControleurAnonyme extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
