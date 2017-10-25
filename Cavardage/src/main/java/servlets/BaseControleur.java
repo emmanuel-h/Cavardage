edb4fc169143f5f60d0b4938747dcd45da4a4900
Emmanuel H <emmanuelh@openmailbox.org>
2017-10-25 11:17:45 +0200

    Aie ma France

--- Cavardage/src/main/java/servlets/BaseControleur.java
+++ Cavardage/src/main/java/servlets/BaseControleur.java
@@ -1,5 +1,7 @@
 package servlets;
 
+import ejbs.MaFacade;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,8 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("BaseControler")
-public class BaseControler extends HttpServlet {
+@WebServlet("BaseControleur")
+public class BaseControleur extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
