2679b6539e0b1d6e40728944a714d7599cec7d7f
martin2994 <martin2994@hotmail.fr>
2017-10-25 18:44:23 +0200

    suite jsp accueil + fonction et classes anonymes

--- CavardageWeb/src/main/java/servlets/ControleurAnonyme.java
+++ CavardageWeb/src/main/java/servlets/ControleurAnonyme.java
@@ -1,5 +1,8 @@
 package servlets;
 
+import ejbs.MaFacadeAnonyme;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +13,9 @@ import java.io.IOException;
 @WebServlet("ControleurAnonyme")
 public class ControleurAnonyme extends HttpServlet {
 
+    @EJB
+    MaFacadeAnonyme ejb;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
@@ -17,6 +23,22 @@ public class ControleurAnonyme extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String test = request.getParameter("afaire");
+        if(null == test){
+            request.getRequestDispatcher("/WEB-INF/accueil.jsp")
+                    .forward(request, response);
+        }else{
+            switch (test){
+                case "connexion":
+                    request.getRequestDispatcher("/WEB-INF/accueil.jsp")
+                            .forward(request, response);
+                    break;
+                case "inspcription":
+                    request.getRequestDispatcher("/WEB-INF/inscription.jsp")
+                            .forward(request, response);
+                    break;
+                default: break;
+            }
+        }
     }
 }
---
+++ CavardageWeb/src/main/webapp/WEB-INF/inscription.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rety
+  Date: 25/10/17
+  Time: 18:35
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Cavardage ~ Inscription ~</title>
+</head>
+<body>
+
+</body>
+</html>
